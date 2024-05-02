6c6b9f6b828d5ae9cc4aaa7cde56cc895b1b943e
VulpoiuPetru <petru.vulpoiu@student.unitbv.ro>
2024-05-02 16:48:11 +0300

    Create wolf

--- Museum/Museum/Museum.cpp
+++ Museum/Museum/Museum.cpp
@@ -457,6 +457,8 @@ void renderMonkey();
 void renderPanther(const Shader& shader);
 void renderPanther();
 
+void renderWolf(const Shader& shader);
+void renderWolf();
 
 void renderSavannahTree(const Shader& shader);
 void renderSavannahTree();
@@ -529,6 +531,8 @@ int main(int argc, char** argv)
 
 	unsigned int monkeyTexture = CreateTexture(strExePath + "\\Museum\\Animals\\Monkey1\\Gorilla_Bake1_PBR StoA_Diffuse.png");
 	unsigned int pantherTexture = CreateTexture(strExePath + "\\Museum\\Animals\\Panther\\panther.jpg");
+
+	unsigned int wolfTexture = CreateTexture(strExePath + "\\Museum\\Animals\\Wolf1\\WOLF.png");
 
 
 	unsigned int savannahGroundTexture = CreateTexture(strExePath + "\\Museum\\Walls\\SavannahGround\\test.jpg");
@@ -753,6 +757,18 @@ int main(int argc, char** argv)
 		glCullFace(GL_BACK);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
+
+		glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, wolfTexture);
+		glEnable(GL_CULL_FACE);
+		glCullFace(GL_FRONT);
+		renderWolf(shadowMappingDepthShader);
+		glCullFace(GL_BACK);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 		// reset viewport
 		glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -859,6 +875,13 @@ int main(int argc, char** argv)
 		glBindTexture(GL_TEXTURE_2D, depthMap);
 		glDisable(GL_CULL_FACE);
 		renderPanther(shadowMappingShader);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, wolfTexture);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, depthMap);
+		glDisable(GL_CULL_FACE);
+		renderWolf(shadowMappingShader);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
@@ -2235,6 +2258,115 @@ void renderPanther()
 	glBindVertexArray(pantherVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, pantherVBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, pantherEBO);
+	int indexArraySize;
+	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
+	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+
+unsigned int indicesW[72000];
+objl::Vertex verW[2000000];
+
+GLuint wolfVAO, wolfVBO, wolfEBO;
+
+void renderWolf(const Shader& shader)
+{
+	//cheetah
+
+	glm::mat4 model;
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(-7.5f, 0.0f, 2.5f));
+	model = glm::scale(model, glm::vec3(6.f));
+	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+	// Adaugarea rota?iei spre dreapta
+	//float rotationAngle = 170.0f; // Rotire spre dreapta
+	//model = glm::rotate(model, glm::radians(rotationAngle), glm::vec3(0.0f, 0.0f, 1.0f)); // Rotire spre dreapta pe axa OZ
+	float rotationAngle1 = 90.0f; // Rotire spre dreapta
+	model = glm::rotate(model, glm::radians(rotationAngle1), glm::vec3(1.0f, 0.0f, 0.0f)); // Rotire spre dreapta pe axa OZ
+	shader.SetMat4("model", model);
+	renderWolf();
+}
+
+
+
+
+void renderWolf()
+{
+	// initialize (if necessary)
+	if (wolfVAO == 0)
+	{
+
+		std::vector<float> verticess;
+		std::vector<float> indicess;
+
+
+
+		Loader.LoadFile("..\\Museum\\Animals\\Wolf1\\WOLF.obj");
+		objl::Mesh curMesh = Loader.LoadedMeshes[0];
+		int size = curMesh.Vertices.size();
+		objl::Vertex v;
+		const float scaleFactor = 0.5f; // factorul de scalare
+		for (int j = 0; j < curMesh.Vertices.size(); j++)
+		{
+			v.Position.X = (float)curMesh.Vertices[j].Position.X * scaleFactor;
+			v.Position.Y = (float)curMesh.Vertices[j].Position.Y * scaleFactor;
+			v.Position.Z = (float)curMesh.Vertices[j].Position.Z * scaleFactor;
+			v.Normal.X = (float)curMesh.Vertices[j].Normal.X;
+			v.Normal.Y = (float)curMesh.Vertices[j].Normal.Y;
+			v.Normal.Z = (float)curMesh.Vertices[j].Normal.Z;
+			v.TextureCoordinate.X = (float)curMesh.Vertices[j].TextureCoordinate.X;
+			v.TextureCoordinate.Y = (float)curMesh.Vertices[j].TextureCoordinate.Y;
+
+
+			verW[j] = v;
+		}
+		for (int j = 0; j < verticess.size(); j++)
+		{
+			vertices[j] = verticess.at(j);
+		}
+
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+
+			indicess.push_back((float)curMesh.Indices[j]);
+
+		}
+		for (int j = 0; j < curMesh.Indices.size(); j++)
+		{
+			indicesW[j] = indicess.at(j);
+		}
+
+		glGenVertexArrays(1, &wolfVAO);
+		glGenBuffers(1, &wolfVBO);
+		glGenBuffers(1, &wolfEBO);
+		// fill buffer
+		glBindBuffer(GL_ARRAY_BUFFER, wolfVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(verW), verW, GL_DYNAMIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, wolfEBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesW), &indicesW, GL_DYNAMIC_DRAW);
+		// link vertex attributes
+		glBindVertexArray(wolfVAO);
+		glEnableVertexAttribArray(0);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+	}
+	// render Cube
+	glBindVertexArray(wolfVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, wolfVBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, wolfEBO);
 	int indexArraySize;
 	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &indexArraySize);
 	glDrawElements(GL_TRIANGLES, indexArraySize / sizeof(unsigned int), GL_UNSIGNED_INT, 0);
